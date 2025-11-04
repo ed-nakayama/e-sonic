3c7c61d3c30de4ab704af357a336010a6fb747fb
ed-nakayama <nakayama@e-d.co.jp>
2025-11-04 19:46:49 +0900

    エラーview追加

---
+++ resources/views/contact_error.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="sdw"></div>
+<!-- content --><!-- InstanceBeginEditable name="EditRegion3" -->
+<div class="wrapper row3">
+  <div id="container">
+    <div class="three_quarter">
+      <div id="respond">
+        <h2>お問い合わせ</h2>
+        <div class="form-input clear"> ロボット確認エラー<br>
+          </div>
+        <div class="clear"></div>
+      </div>
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+
+@endsection
